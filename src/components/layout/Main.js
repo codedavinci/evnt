4588f4c678144aae356499a45d80d456b8fa9a1e
Edivan Silva <codedavinci.now@gmail.com>
2020-02-05 10:15:09 -0500

    feat(Layout): Set up the structure of layout components ( Header, Content, Footer)

---
+++ src/components/layout/Main.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import Header from './Header'
+import Sider from './Sider'
+import Footer from './Footer'
+
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+`
+
+const Content = styled.div`
+  background-color: red;
+`
+
+const Main = ({ children }) => {
+  return (
+    <Container>
+      <Sider />
+      {/* <Content>Foo </Content> */}
+    </Container>
+  )
+}
+
+Main.propTypes = {
+
+}
+
+export default Main
